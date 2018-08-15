a43016ac043321e9a1e41f722c8ee7453866f776
Milos Vulic <milos_vulic97@yahoo.com>
2018-08-15 23:28:28 +0200

    Resources 1.2

--- src/main/java/se201/projekat/Main.java
+++ src/main/java/se201/projekat/Main.java
@@ -1,14 +1,21 @@
 package se201.projekat;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 
-
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("Ok");
+        Parent root = FXMLLoader.load(getClass().getResource("FirstView.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/se201/projekat/introduction/FirstViewController.java
+++ src/main/java/se201/projekat/introduction/FirstViewController.java
@@ -1,5 +1,30 @@
 package se201.projekat.introduction;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FirstViewController {
 
+    public void setOnCreateNewAccount(ActionEvent actionEvent) throws Exception {
+
+        //TODO  Promeni ime controllera tj kako oces da ti se zove controller za panel gde addujes novi kontakt
+        Parent root = FXMLLoader.load(getClass().getResource("Nesto.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("JavaFX and Maven");
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void setOnOpenAddressBook(ActionEvent actionEvent) {
+    }
+
+    public void setOnCheckTheAnalysis(ActionEvent actionEvent) {
+    }
 }
