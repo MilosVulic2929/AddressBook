4cc8ad8bf3e21d970ed1b252c1e2db9f8a73401a
FilipRistic2922 <filip.ristic.2922@gmail.com>
2018-08-15 19:11:45 +0200

    Initial commit

---
+++ src/main/java/se201/projekat/Main.java
@@ -0,0 +1,18 @@
+package se201.projekat;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    public void start(Stage primaryStage) throws Exception {
+
+        System.out.println("Ok");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/test/java/se201/projekat/MainTest.java
@@ -0,0 +1,14 @@
+package se201.projekat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void test(){
+        assertEquals(5,5);
+    }
+
+}
